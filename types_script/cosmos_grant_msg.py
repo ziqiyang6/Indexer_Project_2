efeffa438b9cef580666f57315e57adae54f326b
ziqiyang6 <137822064+ziqiyang6@users.noreply.github.com>
2024-05-22 00:30:39 +0800

    Add files via upload

---
+++ types_script/cosmos_grant_msg.py
@@ -0,0 +1,94 @@
+# !/usr/bin/python3
+'''**********************************************************************************
+                                                                                    *
+Project Name: cosmos_grant_msg.py                                *
+                                                                                    *
+Programming Language: Python 3.11                                                   *
+                                                                                    *
+Libraries: json                                                                     *
+                                                                                    *
+Creater Name: Ziqi Yang                                                             *
+                                                                                    *
+Published Date: 4/15/2024                                                           *
+                                                                                    *
+Version: 1.0                                                                        *
+                                                                                    *
+Version: 1.1                                                                        *
+For 'cursor.execute' command, there is 'try' and 'except' to catch UniqueViolation  *
+And if UniqueViolation happens, there will be search query to search needed value   *
+New package: psycopg2 now applies on this script                                    *
+New column 'comment' for transaction table has been added                           *                                                                                    *
+                                                                                    *
+                                                                                    *
+                                                                                    *
+**********************************************************************************'''
+
+#    Scripts start below
+from functions import create_connection
+import json
+from psycopg2 import errors
+
+def main(tx_id, num, tx_type, message, ids):
+
+    # import the login info for psql from 'info.json'
+    with open('info.json', 'r') as f:
+        info = json.load(f)
+
+    db_name = info['psql']['db_name']
+    db_user = info['psql']['db_user']
+    db_password = info['psql']['db_password']
+    db_host = info['psql']['db_host']
+    db_port = info['psql']['db_port']
+
+    connection = create_connection(db_name, db_user, db_password, db_host, db_port)
+    cursor = connection.cursor()
+    try:
+
+
+        # Edit the query that will be loaded to the database
+        query = """
+                INSERT INTO cosmos_grant_msg (tx_id, tx_type, send_address_id, receive_address_id, authorizationtype, expiration, max_tokens, authorization_type, msg, message_info, comment) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING message_id;
+                """
+
+        # Define the values
+        authorizationtype = message['grant']['authorization']['@type']
+        expiration = message['grant']['expiration']
+        comment = f'This is number {num} message in transaction {tx_id}'
+
+        # Condition 1: if there is more than two element in authorization, load like this
+        if len(message['grant']['authorization']) > 2 :
+            # If max tokens is NULL, make it as '', avoiding the error
+            if message['grant']['authorization']['max_tokens'] == None:
+                max_tokens = ''
+            else:
+                max_tokens = message['grant']['authorization']['max_tokens']
+            authorization_type = message['grant']['authorization']['authorization_type']
+            msg = ''
+            message_info = json.dumps(message)
+            values = (tx_id, tx_type, ids['granter_id'], ids['grantee_id'], authorizationtype, expiration, max_tokens, authorization_type, msg, message_info, comment )
+            cursor.execute(query, values)
+            message_id = cursor.fetchone()[0]
+            # In this condition, more info will be loaded to allow list table
+            for address in message['grant']['authorization']['allow_list']['address']:
+
+                cursor.execute('INSERT INTO cosmos_grant_allowlist (message_id, addresses) VALUES (%s, %s); ', (message_id, address))
+        # Condition 2: If not , load like this
+        else:
+            authorization_type = ''
+
+            msg = message['grant']['authorization']['msg']
+            max_tokens = ''
+            message_info = json.dumps(message)
+            values = (tx_id, tx_type, ids['granter_id'], ids['grantee_id'], authorizationtype, expiration, max_tokens, authorization_type, msg, message_info, comment  )
+            cursor.execute(query, values)
+
+        connection.commit()
+        connection.close()
+    except KeyError:
+        print(f'KeyError happens in type {tx_type}')
+    except errors.UniqueViolation as e:
+        pass
+
+if __name__ == '__main__':
+    main(tx_id, tx_type, message, ids)
---
+++ types_script/ibc_channelopentry_msg.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+'''**********************************************************************************
+                                                                                    *
+Project Name: ibc_channelopentry_msg.py                                *
+                                                                                    *
+Programming Language: Python 3.11                                                   *
+                                                                                    *
+Libraries: json                                                                     *
+                                                                                    *
+Creater Name: Ziqi Yang                                                             *
+                                                                                    *
+Published Date: 4/15/2024                                                           *
+                                                                                    *
+Version: 1.0                                                                        *
+                                                                                    *
+Version: 1.1                                                                        *
+'version' column in query has been changed to 'version_num'.                              *                                                                                    *
+                                                                                    *
+Version: 1.2                                                                        *
+For 'cursor.execute' command, there is 'try' and 'except' to catch UniqueViolation  *
+And if UniqueViolation happens, there will be search query to search needed value   *
+New package: psycopg2 now applies on this script                                    *
+New column 'comment' for transaction table has been added                           *                                                                                    *
+                                                                                    *
+**********************************************************************************'''
+
+#    Scripts start below
+from functions import create_connection
+import json
+from psycopg2 import errors
+
+def main(tx_id,num, tx_type, message):
+
+    # import the login info for psql from 'info.json'
+    with open('info.json', 'r') as f:
+        info = json.load(f)
+
+    db_name = info['psql']['db_name']
+    db_user = info['psql']['db_user']
+    db_password = info['psql']['db_password']
+    db_host = info['psql']['db_host']
+    db_port = info['psql']['db_port']
+
+    connection = create_connection(db_name, db_user, db_password, db_host, db_port)
+    cursor = connection.cursor()
+
+    try:
+        # Edit the query that will be loaded to the database
+        query = """
+                INSERT INTO ibc_channelopentry_msg (tx_id, tx_type, port_id, previous_channel_id, channel_state, channel_ordering, counterparty_port_id, counterparty_channel_id, connection_hops, version_num, counterparty_version, proof_init, proof_height_revision_number, proof_height_revision_height, signer, message_info, comment) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """
+
+        # Define the values
+        port_id = message['port_id']
+        previous_channel_id = message['previous_channel_id']
+        channel_state = message['channel']['state']
+        channel_ordering = message['channel']['ordering']
+        counterparty_port_id = message['channel']['counterparty']['port_id']
+        counterparty_channel_id = message['channel']['counterparty']['channel_id']
+        connection_hops = message['channel']['connection_hops'][0]
+        version = message['channel']['version']
+        counterparty_version = message['counterparty_version']
+        proof_init = message['proof_init']
+        proof_height_revision_number = message['proof_height']['revision_number']
+        proof_height_revision_height = message['proof_height']['revision_height']
+        signer = message['signer']
+        message = json.dumps(message)
+        comment = f'This is number {num} message in transaction {tx_id}'
+
+        values = (tx_id, tx_type, port_id, previous_channel_id, channel_state, channel_ordering, counterparty_port_id, counterparty_channel_id, connection_hops, version, counterparty_version, proof_init, proof_height_revision_number, proof_height_revision_height, signer, message,comment)
+        cursor.execute(query, values)
+
+        connection.commit()
+        connection.close()
+
+    except KeyError:
+        print(f'KeyError happens in type {tx_type}')
+    except errors.UniqueViolation as e:
+        pass
+
+if __name__ == '__main__':
+    main(tx_id, tx_type, message)
