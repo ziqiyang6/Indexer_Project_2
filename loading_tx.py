d0e276808d1d1222ea1694af681a09e7ac3b7aae
Thomas Wang <tmwang7324@gmail.com>
2024-06-18 14:02:36 -0500

    6/18/2024 created a logger to catch the block that causes an error when running loading_tx

--- loading_tx.py
+++ loading_tx.py
@@ -36,6 +36,8 @@ import json
 import importlib
 import address_load
 import os
+import logging
+import traceback
 from functions import check_file
 from functions import create_connection
 from functions import decode_tx
@@ -43,6 +45,13 @@ from functions import hash_to_hex
 from functions import new_type
 from pathlib import Path
 from psycopg2 import errors
+
+error_logger = logging.getLogger('error')
+error_logger.setLevel(logging.ERROR)
+
+error_handler = logging.FileHandler('error.log')
+error_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+error_logger.addHandler(error_handler)
 
 # import the login info for psql from 'info.json'
 with open('info.json', 'r') as f:
@@ -66,23 +75,28 @@ output_path = os.getenv('txt')
 content = check_file(file_path, file_name)
 num = os.getenv('x')
 # decoded_response = decode_tx(content['block']['data']['txs'][int(num)])
-transaction_string = content['block']['data']['txs'][int(num)]
-decoded_response = decode_tx(transaction_string)
-tx_hash = hash_to_hex(transaction_string)
-chain_id = content['block']['header']['chain_id']
-height = content['block']['header']['height']
-search_query = f"SELECT block_id FROM blocks WHERE height = '{height}'" # Search the block hash from the block
-cursor.execute(search_query)
-result = cursor.fetchall()
-block_id = result[0][0]
-memo = decoded_response['tx']['body']['memo']
-fee_denom = decoded_response['tx']['auth_info']['fee']['amount'][0]['denom']
-fee_amount = decoded_response['tx']['auth_info']['fee']['amount'][0]['amount']
-gas_limit = decoded_response['tx']['auth_info']['fee']['gas_limit']
-created_time = content['block']['header']['time']
-order = int(num) + 1
-comment = f'This is number {order} transaction in BLOCK {height}'
-tx_info = json.dumps(decoded_response)
+try:
+    transaction_string = content['block']['data']['txs'][int(num)]
+    decoded_response = decode_tx(transaction_string)
+    tx_hash = hash_to_hex(transaction_string)
+    chain_id = content['block']['header']['chain_id']
+    height = content['block']['header']['height']
+    search_query = f"SELECT block_id FROM blocks WHERE height = '{height}'" # Search the block hash from the block
+    cursor.execute(search_query)
+    result = cursor.fetchall()
+    block_id = result[0][0]
+    memo = decoded_response['tx']['body']['memo']
+    fee_denom = decoded_response['tx']['auth_info']['fee']['amount'][0]['denom']
+    fee_amount = decoded_response['tx']['auth_info']['fee']['amount'][0]['amount']
+    gas_limit = decoded_response['tx']['auth_info']['fee']['gas_limit']
+    created_time = content['block']['header']['time']
+    order = int(num) + 1
+    comment = f'This is number {order} transaction in BLOCK {height}'
+    tx_info = json.dumps(decoded_response)
+except Exception as e:
+    error_logger.error(f"Error with loading block info in block " + file_name)
+    error_logger.error(traceback.format_exc())
+    raise
 
 # Edit the query that will be loaded to the database
 query = """
