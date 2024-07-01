a8da9f37ea6a68154e10a5834b333c5e741d618e
Thomas Wang <tmwang7324@gmail.com>
2024-07-01 10:08:39 -0500

    7/1/2024 added new files

---
+++ find_keyword.py
@@ -0,0 +1,35 @@
+import os
+
+rootDir = os.getcwd()
+
+def scan_file(filename, dirname):
+    print(os.path.join(dirname,filename))
+    contains = False
+    
+    
+
+    if("Error with loading block info" in filename):
+        contains = True
+    else:
+        with open(os.path.join(dirname,filename)) as f:
+            lines = f.readlines()
+            for l in lines:
+                #print(l)
+                if("Error with loading block info" in l):
+                    contains = True
+                    print(l)
+                    break
+
+    if contains:
+        print("yes")
+        
+
+
+
+
+if __name__ == '__main__':
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        for fname in fileList:
+            if(os.path.join(dirName,fname) == "/home/tw2623/Indexer-Project/ERD.pdf"):
+                continue
+            scan_file(fname, dirName)
